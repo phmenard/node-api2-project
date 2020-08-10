51ce7f8e02e4e7e8f2669528bedc61091da2c1cf
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-08-10 02:25:16 -0400

    fixed error msg

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -65,7 +65,7 @@ router.get("/api/posts", (req, res) => {
 })
 
 router.get("/api/posts/:id", (req, res) => {
-
+    try {
     posts.findById(req.params.id)
         .then((post) => {
             if (post.length === 0) { throw (404) }
@@ -73,8 +73,11 @@ router.get("/api/posts/:id", (req, res) => {
         })
         .catch((error) => {
             console.log(error)
-            res.status(500).json({ error: "The post information could not be retrieved." })
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
         })
+    }catch(e){
+        res.status(500).json({ error: "The comments information could not be retrieved." })
+    }
 
 })
 
