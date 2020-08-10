26809133ee458a415bc7fe259348809f07da586b
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-08-10 02:20:28 -0400

    added put and delete

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -33,6 +33,25 @@ router.post("/api/posts/:id/comments", (req, res) => {
 		})
 })
 
+router.put("/api/posts/:id", (req, res) => {
+	if (!req.body.title || !req.body.contents) {
+		return res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+	}
+
+	posts.update(req.params.id, req.body)
+		.then((post) => {
+			if (post) {
+				res.status(200).json(post)
+			} else {
+				res.status(404).json({ message: "The post with the specified ID does not exist." })
+			}
+		})
+		.catch((error) => {
+			console.log(error)
+			res.status(500).json({ error: "The post information could not be modified." })
+		})
+})
+
 router.get("/api/posts", (req, res) => {
     console.log(req.query)
     posts.find(req.query)
@@ -72,6 +91,23 @@ router.get("/api/posts/:id/comments", (req, res) => {
     }catch(e) {
         res.status(500).json({ error: "The comments information could not be retrieved." })
     }
+})
+
+router.delete("/api/posts/:id", (req, res) => {
+	posts.remove(req.params.id)
+		.then((post) => {
+			if (post > 0) {
+				res.status(200).json({
+					message: "The post has been nuked",
+				})
+			} else {
+				res.status(404).json({ message: "The post with the specified ID does not exist." })
+			}
+		})
+		.catch((error) => {
+			console.log(error)
+			res.status(500).json({ error: "The post could not be removed" })
+		})
 })
 
 module.exports = router;
